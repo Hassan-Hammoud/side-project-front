68d6a6f584aebb46dbbea475fa4ec4de51c03d68
Hassan Hammoud <hassanhammoud2000@gmail.com>
2023-04-17 10:04:46 +0300

    start Footer

--- e-commerce/src/components/Footer/Footer.js
+++ e-commerce/src/components/Footer/Footer.js
@@ -2,10 +2,31 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
-      Footer
-    </div>
-  )
+    <>
+      <footer className="py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-5">
+              <div className="footer-top-data d-flex gap-30 align-items-center">
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-3"></footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <p className="text-center mb-0 text-white">
+                &copy; {new Date().getFullYear()}; Powered by developer...
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
 
 export default Footer
